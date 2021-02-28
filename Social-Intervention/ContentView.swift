13071d9e417355611b6fd56e37f5acfef6bdafd3
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2021-02-28 12:45:04 -0800

    Initial Commit

---
+++ Social-Intervention/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Social-Intervention
+//
+//  Created by Jackson Chui on 2/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
