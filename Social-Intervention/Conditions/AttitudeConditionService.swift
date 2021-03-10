37ec72bf821f061de5f102d482d22f4f75ff3e70
Jackson Chui <14085209+jacksonchui@users.noreply.github.com>
2021-03-09 16:40:29 -0800

    Segmented functionalities into extensions

--- Social-Intervention/Conditions/AttitudeConditionService.swift
+++ Social-Intervention/Conditions/AttitudeConditionService.swift
@@ -18,12 +18,11 @@ public class ConditionSession {
 }
 
 public class AttitudeConditionService: ConditionService {
-        
     private(set) var attitudeService: AttitudeMotionService
     
     private(set) var currentPeriodTime: TimeInterval = 0
     private(set) var targetAttitude: Attitude?
-    private var timeInterval: TimeInterval
+    private(set) var timeInterval: TimeInterval
     private(set) var records = [Attitude]()
     
     static let progressThreshold = 0.7
@@ -31,31 +30,21 @@ public class AttitudeConditionService: ConditionService {
     init(with attitudeService: AttitudeMotionService, updateEvery timeInterval: TimeInterval) {
         self.attitudeService = attitudeService
         self.timeInterval = timeInterval
-        
     }
-    
+}
+
+extension AttitudeConditionService {
     public func check(completion: @escaping CheckCompletion) {
         attitudeService.checkAvailability(completion: completion)
     }
-    
+}
+
+extension AttitudeConditionService {
     public func start(completion: @escaping StartCompletion) {
         currentPeriodTime = 0
         attitudeService.startUpdates(updatingEvery: timeInterval) { [weak self] result in
             guard let self = self else { return }
             self.record(result: result, completion: completion)
-        }
-    }
-    
-    public func stop(completion: @escaping StopCompletion) {
-        attitudeService.stopUpdates {[weak self] error in
-            guard let self = self else { return }
-            
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            completion(.success(progressAboveThreshold: self.progressAboveThreshold))
-            self.resetConditionServiceState()
         }
     }
     
@@ -73,7 +62,7 @@ public class AttitudeConditionService: ConditionService {
         }
     }
     
-    public var progress: Double {
+    private var progress: Double {
         guard let record = records.last,
               let targetAttitude = targetAttitude else {
             return 1.0
@@ -81,7 +70,32 @@ public class AttitudeConditionService: ConditionService {
         return record.progress(till: targetAttitude)
     }
     
-    public var progressAboveThreshold: Double {
+    private var randomRadian: Double {
+        let sigFigures = 2
+        return Double.random(in: -Double.pi/2...Double.pi/2).truncate(places: sigFigures)
+    }
+    
+    private var randomAttitude: Attitude {
+        let newAttitude = Attitude(roll: randomRadian, pitch: randomRadian, yaw: randomRadian)
+        return newAttitude != records.first ? newAttitude : self.randomAttitude
+    }
+}
+
+extension AttitudeConditionService {
+    public func stop(completion: @escaping StopCompletion) {
+        attitudeService.stopUpdates {[weak self] error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            completion(.success(progressAboveThreshold: self.progressAboveThreshold))
+            self.resetConditionServiceState()
+        }
+    }
+    
+    private var progressAboveThreshold: Double {
         guard let targetAttitude = targetAttitude else { return 0.0 }
         let progresses = records.map { $0.progress(till: targetAttitude) }
         let recordsAboveThreshold = progresses.reduce(0) { sum, progress in sum + (progress >= AttitudeConditionService.progressThreshold ? 1 : 0) }
@@ -94,16 +108,6 @@ public class AttitudeConditionService: ConditionService {
         currentPeriodTime = 0
         records = []
         targetAttitude = nil
-    }
-    
-    private var randomRadian: Double {
-        let sigFigures = 2
-        return Double.random(in: -Double.pi/2...Double.pi/2).truncate(places: sigFigures)
-    }
-    
-    private var randomAttitude: Attitude {
-        let newAttitude = Attitude(roll: randomRadian, pitch: randomRadian, yaw: randomRadian)
-        return newAttitude != records.first ? newAttitude : self.randomAttitude
     }
 }
 
